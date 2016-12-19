c6d54d18c32ffdddebf6fdd326de2df794e0b131
sandip-android <sandippatel.android@gmail.com>
2016-12-19 16:57:10 +0530

    Update AppConstant.java

--- AppConstant.java
+++ AppConstant.java
@@ -28,29 +28,4 @@ public class AppConstant {
     public static final String SENT_TOKEN_TO_SERVER = "sentTokenToServer";
     public static final String REGISTRATION_COMPLETE = "registrationComplete";
     public static final String PREF_GCM_DEVICE_ID = "gcm_device_id";
-
-    public static final String PREF_VIBRATE = "vibrate";
-    public static final String PREF_RINGTONE = "ringtone";
-    public static final String PREF_THEME_COLOR = "theme_color";
-    public static final String PREF_NOTIFICATION_STATUS = "notification_status";
-    public static final String PREF_CUSTOM_NOTIFICATION_STATUS = "custom_notification_status";
-    public static final String PREF_DATE_FORMAT = "date_format";
-    public static final String PREF_TIME_FORMAT = "time_format";
-    public static final String PREF_START_DAY = "start_day";
-    public static final String PREF_NEW_TASK_STATUS = "new_task_status";
-    public static final String PREF_IS_APP_DEFAULT = "is_app_default";
-
-    public static final String COLOR_RED = "color_red";
-    public static final String COLOR_BLUE = "color_blue";
-    public static final String COLOR_BLACK = "color_balck";
-    public static final String COLOR_GREEN = "color_green";
-    public static final String COLOR_PINK = "color_pink";
-    public static final String COLOR_GRAY = "color_gray";
-
-    public static final String FRAGMENT_HOME = "home_fragment";
-    public static final String FRAGMENT_HISTORY = "history_fragment";
-    public static final String FRAGMENT_SETTING = "setting_fragment";
-    public static final String FRAGMENT_BACKUP_RESTORE = "backup_restore_fragment";
-    public static final String FRAGMENT_FESTIVALS = "festivals_fragment";
-
 }
